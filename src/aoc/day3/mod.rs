e00bfc9584f6e06390cb5b4828b8ee430292e725
Sathis Kumar Muthusamy <smuthusamy@atlassian.com>
2019-12-03 14:26:16 +0530

    Add day 3 part2

--- src/aoc/day3/mod.rs
+++ src/aoc/day3/mod.rs
@@ -1,8 +1,8 @@
 use crate::aoc::utils::Input;
 use std::collections::HashSet;
 
-fn get_points(line: Vec<String>) -> HashSet<(i32, i32)>{
-    let mut points = HashSet::new();
+fn get_points(line: Vec<String>) -> Vec<(i32, i32)> {
+    let mut points = vec![];
 
     let mut x = 0;
     let mut y = 0;
@@ -11,40 +11,49 @@ fn get_points(line: Vec<String>) -> HashSet<(i32, i32)>{
         match op {
             "R" => {
                 let new_x = x + size.parse::<i32>().unwrap();
-                add_points(&mut points, x, y, new_x, y);
+                add_points(&mut points, x + 1, y, new_x, y);
                 x = new_x;
-            },
+            }
             "L" => {
                 let new_x = x - size.parse::<i32>().unwrap();
-                add_points(&mut points, new_x, y, x, y);
+                add_points(&mut points, x - 1, y, new_x, y);
                 x = new_x;
-            },
+            }
             "U" => {
                 let new_y = y + size.parse::<i32>().unwrap();
-                add_points(&mut points, x, y, x, new_y);
+                add_points(&mut points, x, y + 1, x, new_y);
                 y = new_y;
-            },
+            }
             "D" => {
                 let new_y = y - size.parse::<i32>().unwrap();
-                add_points(&mut points, x, new_y, x, y);
+                add_points(&mut points, x, y - 1, x, new_y);
                 y = new_y;
-            },
+            }
             _ => panic!()
         }
     }
     points
 }
 
-fn add_points(points: &mut HashSet<(i32, i32)>, x_start: i32, y_start: i32, x_end: i32, y_end: i32) -> &mut HashSet<(i32, i32)> {
-
+fn add_points(points: &mut Vec<(i32, i32)>, x_start: i32, y_start: i32, x_end: i32, y_end: i32) -> &mut Vec<(i32, i32)> {
     if x_start == x_end {
-        for y in y_start..y_end + 1 {
-            points.insert((x_start, y));
+        let step = if y_start > y_end { -1 } else { 1 };
+        let mut y = y_start;
+
+        while y != y_end {
+            points.push((x_start, y));
+            y = y + step;
         }
+        points.push((x_start, y));
     } else {
-        for x in x_start..x_end + 1 {
-            points.insert((x, y_start));
+        let step = if x_start > x_end { -1 } else { 1 };
+        let mut x = x_start;
+
+        while x != x_end {
+            points.push((x, y_start));
+            x = x + step;
         }
+        points.push((x, y_start));
     }
 
     points
@@ -61,8 +70,11 @@ pub async fn part1() -> String {
     let points1 = get_points(line1);
     let points2 = get_points(line2);
 
+    let set1: HashSet<(i32, i32)> = points1.iter().cloned().collect();
+    let set2: HashSet<(i32, i32)> = points2.iter().cloned().collect();
+
     let mut min = i32::max_value();
-    for (x, y) in points1.intersection(&points2) {
+    for (x, y) in set1.intersection(&set2) {
         let dist = i32::abs(*x) + i32::abs(*y);
 
         //Lets skip point (0, 0)
@@ -74,5 +86,28 @@ pub async fn part1() -> String {
 }
 
 pub async fn part2() -> String {
-    unimplemented!()
+    let input = Input {
+        path: "src/aoc/day3/input.txt".to_string()
+    };
+    let lines = input.read().await;
+    let mut line1: Vec<String> = lines.first().unwrap().split(",").map(|s| s.to_string()).collect();
+    let mut line2: Vec<String> = lines.last().unwrap().split(",").map(|s| s.to_string()).collect();
+
+    let points1 = get_points(line1);
+    let points2 = get_points(line2);
+
+    let set1: HashSet<(i32, i32)> = points1.iter().cloned().collect();
+    let set2: HashSet<(i32, i32)> = points2.iter().cloned().collect();
+
+    let mut min = usize::max_value();
+    for (x, y) in set1.intersection(&set2) {
+        let dist = points1.iter().position(|(x2, y2)| x == x2 && y == y2).unwrap() +
+            points2.iter().position(|(x2, y2)| x == x2 && y == y2).unwrap() + 2;
+
+        //Lets skip point (0, 0)
+        if dist < min && dist > 0 {
+            min = dist;
+        }
+    }
+    min.to_string()
 }
